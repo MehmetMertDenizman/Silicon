8d6d09a1aa1ae31677d06fd573436fc4d058e38c
Mehmet Mert Denizman <amorylorch@gmail.com>
2022-08-07 22:39:39 +0300

    Add files via upload

---
+++ ogrenci/Program.cs
@@ -0,0 +1,52 @@
+﻿using ogrenci;
+using System.Linq;
+
+List<student> stu = new List<student>();
+
+
+while (true)
+{
+    Console.WriteLine("Öğrenci Yönetim Uygulaması\n" +
+    "1- Öğrenci Ekle (E)\n" +
+    "2- Öğrenci Listele (L)\n" +
+    "3- Öğrenci Sil (S)\n" +
+    "4- Çıkış (X)\n");
+    Console.Write("Seçim yapın: ");
+    string secim = Console.ReadLine();
+    Console.WriteLine("Seçiminiz: " + secim);
+    if (secim == "1" || secim.ToUpper() == "E")
+    {
+        stu.Add(new student());
+        Console.WriteLine("Öğrenciyi kaydetmek istediğinize emin misiniz? E/H"); string check = Console.ReadLine(); if (check == "1" || check.ToUpper() == "E")
+        {
+            Console.WriteLine("Öğrenci eklendi");
+        }
+        else
+        { stu.Remove(stu.Last()); Console.WriteLine("Öğrenci eklenmedi"); }
+    }
+    //case "1": case "E": student ogrenci = new student(); //ogrenci.ogrenciEkle(); break; NORMAL YOL
+    else if (secim == "2" || secim.ToUpper() == "L")
+    {
+        Console.WriteLine(stu.Count());
+        stu.ForEach(
+            studen => Console.WriteLine($"Öğrenci adı: {studen.ad}, öğrenci no: {studen.no}, soyadı: {studen.soyAd}, şube:{studen.sube}"));
+    }
+
+    else if (secim == "3" || secim.ToUpper() == "S")
+    {
+        stu.ForEach(
+          studen => Console.WriteLine($"Öğrenci adı: {studen.ad}, öğrenci no: {studen.no}, soyadı: {studen.soyAd}, şube:{studen.sube}"));
+        Console.WriteLine("sil no: ");
+        string sil = Console.ReadLine();
+        bool a = stu.Exists(x => x.no.Equals(sil));
+        if (  a ==true) 
+        {
+            stu.RemoveAll(x => x.no.Equals(sil));
+               
+                
+            Console.WriteLine("Okkey"); 
+
+        }
+        else { Console.WriteLine("yok"); }
+    }
+}
---
+++ ogrenci/student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenci
+{
+    public class student
+    {
+        Random random = new Random();
+        public int ID;
+        public string no;
+        public string ad;
+        public string soyAd;
+        public string sube;
+        public student()
+        {
+
+
+            ID = random.Next();
+            Console.WriteLine("ID: " + ID);
+            
+            Console.Write("No: ");
+            no = Console.ReadLine();
+            Console.Write("Adı: ");
+            ad = Console.ReadLine();
+            Console.Write("Soyadı: ");
+            soyAd = Console.ReadLine();
+            Console.Write("Şubesi: ");
+            sube = Console.ReadLine();
+        }
+        public void ogrenciEkle()
+        {
+            Console.WriteLine("ÖĞRENCİ EKLE METHOD");
+            Console.WriteLine("No gir: ");
+            no = Console.ReadLine();
+            Console.WriteLine("Ad gir: ");
+            ad = Console.ReadLine();
+            Console.WriteLine("Soyad gir: ");
+            soyAd = Console.ReadLine();
+            Console.WriteLine("Sube gir: ");
+            sube = Console.ReadLine();
+        }
+    }
+}
