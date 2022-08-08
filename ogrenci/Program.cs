2547da8423a4408c18cce7a673899d2a94f381b7
Mehmet Mert Denizman <amorylorch@gmail.com>
2022-08-08 23:18:23 +0300

    Add files via upload

--- ogrenci/Program.cs
+++ ogrenci/Program.cs
@@ -1,24 +1,31 @@
-﻿using ogrenci;
+﻿
+
+using ogrenci;
 using System.Linq;
 
 List<student> stu = new List<student>();
-
-
-while (true)
+student stuMethod = new student(1);
+int say = 0;
+while (say < 10)
 {
+    say++;
     Console.WriteLine("Öğrenci Yönetim Uygulaması\n" +
     "1- Öğrenci Ekle (E)\n" +
     "2- Öğrenci Listele (L)\n" +
     "3- Öğrenci Sil (S)\n" +
     "4- Çıkış (X)\n");
-    Console.Write("Seçim yapın: ");
+ 
     string secim = Console.ReadLine();
-    Console.WriteLine("Seçiminiz: " + secim);
+    //Console.WriteLine("Seçiminiz: " + secim);
     if (secim == "1" || secim.ToUpper() == "E")
     {
-        stu.Add(new student());
+        Console.WriteLine("Öğrenci ekle ----------");
+
+
+        stu.Add(new student(stu));
         Console.WriteLine("Öğrenciyi kaydetmek istediğinize emin misiniz? E/H"); string check = Console.ReadLine(); if (check == "1" || check.ToUpper() == "E")
         {
+            student.sayac++;
             Console.WriteLine("Öğrenci eklendi");
         }
         else
@@ -27,26 +34,26 @@ while (true)
     //case "1": case "E": student ogrenci = new student(); //ogrenci.ogrenciEkle(); break; NORMAL YOL
     else if (secim == "2" || secim.ToUpper() == "L")
     {
-        Console.WriteLine(stu.Count());
-        stu.ForEach(
-            studen => Console.WriteLine($"Öğrenci adı: {studen.ad}, öğrenci no: {studen.no}, soyadı: {studen.soyAd}, şube:{studen.sube}"));
+
+        stuMethod.ogrenciListele(stu);
+
     }
 
     else if (secim == "3" || secim.ToUpper() == "S")
     {
-        stu.ForEach(
-          studen => Console.WriteLine($"Öğrenci adı: {studen.ad}, öğrenci no: {studen.no}, soyadı: {studen.soyAd}, şube:{studen.sube}"));
-        Console.WriteLine("sil no: ");
-        string sil = Console.ReadLine();
-        bool a = stu.Exists(x => x.no.Equals(sil));
-        if (  a ==true) 
+        if (stu.Count() == 0)
         {
-            stu.RemoveAll(x => x.no.Equals(sil));
-               
-                
-            Console.WriteLine("Okkey"); 
+            Console.Clear();
+            Console.WriteLine("Listede silinecek öğrenci yok.\n\n");
+
 
         }
-        else { Console.WriteLine("yok"); }
+        else { stuMethod.ogrenciSil(stu); }
     }
+    else if (secim == "4" || secim.ToUpper() == "X")
+    {
+
+        break;
+    }
+
 }
--- ogrenci/student.cs
+++ ogrenci/student.cs
@@ -9,38 +9,101 @@ namespace ogrenci
     public class student
     {
         Random random = new Random();
+        public static int sayac = 1;
         public int ID;
         public string no;
         public string ad;
         public string soyAd;
         public string sube;
-        public student()
+
+        public student(List<student> stu)
+        {
+
+
+            ogrenciEkle(stu);
+
+        }
+        public student(int a) { }
+        public void ogrenciEkle(List<student> stu)
         {
 
 
             ID = random.Next();
             Console.WriteLine("ID: " + ID);
-            
+
+            Console.WriteLine(sayac + ". Öğrencinin");
             Console.Write("No: ");
             no = Console.ReadLine();
+            int noInt = int.Parse(no);
+            stu.Exists(x=> x.no.Equals(noInt)); 
+              
+            //if (noInt == check) { }
             Console.Write("Adı: ");
             ad = Console.ReadLine();
             Console.Write("Soyadı: ");
             soyAd = Console.ReadLine();
             Console.Write("Şubesi: ");
             sube = Console.ReadLine();
+
+
         }
-        public void ogrenciEkle()
+        public void ogrenciListele(List<student> stu)
         {
-            Console.WriteLine("ÖĞRENCİ EKLE METHOD");
-            Console.WriteLine("No gir: ");
-            no = Console.ReadLine();
-            Console.WriteLine("Ad gir: ");
-            ad = Console.ReadLine();
-            Console.WriteLine("Soyad gir: ");
-            soyAd = Console.ReadLine();
-            Console.WriteLine("Sube gir: ");
-            sube = Console.ReadLine();
+            Console.Clear();
+            if (stu.Count == 0) { Console.WriteLine("Gösterilecek öğrenci yok.\n\n"); }
+            else
+            {
+                Console.WriteLine("Seçiminiz: 2");
+                Console.WriteLine("Şube    " + "No    " + "Ad Soyad");
+                Console.WriteLine("----------------------------------");
+                stu.ForEach(
+                    studen => Console.WriteLine($"{studen.sube} {studen.no,7} {studen.ad[0].ToString().ToUpper(),7}{studen.ad.Substring(1).ToLower()} {studen.soyAd[0].ToString().ToUpper()}{studen.soyAd.Substring(1).ToLower()}"));
+
+                Console.WriteLine();
+            }
+        }
+        public void ogrenciSil(List<student> stu)
+        {
+
+
+            Console.Clear();
+            Console.WriteLine("Seçiminiz: 3");
+            Console.WriteLine("Silmek istediğiniz öğrencinin");
+            Console.Write("No: ");
+            string sil = Console.ReadLine();
+            int silInt = int.Parse(sil);
+            //stu.Exists(x => x.no.Equals(sil));
+            //stu.ForEach(studen => Console.WriteLine("Senin öğrencin" + stu.FindIndex(x=> x.no==sil)));
+            Console.WriteLine("Şube    " + "No    " + "Ad Soyad");
+            Console.WriteLine("----------------------------------");
+            stu.ForEach(
+              studen =>
+              Console.WriteLine($"{studen.sube} {studen.no,7} {studen.ad,7} {studen.soyAd}"));
+            Console.WriteLine("Öğrenciyi silmek istediğinize emin misiniz? (E/H)");
+            string check = Console.ReadLine();
+            if (check.ToUpper() == "E")
+            {
+                //  stu.ForEach(
+                //studen => 
+                //Console.WriteLine($"{studen.sube} {studen.no,7} {studen.ad,7} {studen.soyAd}"));
+                bool a = stu.Exists(x => x.no.Equals(sil));
+                if (a == true)
+                {
+                    stu.RemoveAll(x => x.no.Equals(sil));
+
+
+                    Console.WriteLine("Öğrenci silindi");
+
+                }
+                else if (a == false) { Console.WriteLine("Böyle bir öğrenci bulunamadı."); }
+            }
+            if (check.ToUpper() == "H") { Console.WriteLine("Öğrenci silinmedi."); }
+
+        }
+        public void SecimAl(string secim)
+        {
+
+            secim = Console.ReadLine();
         }
     }
 }
